d16245f85e482dfa82d1471e0f28e1c22ed46837
Jonathan <iNoles@users.noreply.github.com>
2021-09-20 21:44:38 -0400

    Delete shared/src/iosMain/kotlin/com/jonathansteele/spacexlaunch/shared directory

--- shared/src/iosMain/kotlin/com/jonathansteele/spacexlaunch/shared/DatabaseDriverFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.jonathansteele.spacexlaunch.shared
-
-import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
-
-actual class DatabaseDriverFactory {
-    actual fun createDriver(): SqlNormalizedCacheFactory = SqlNormalizedCacheFactory("apollo.db")
-}
